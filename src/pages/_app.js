b65b7787df37829939c6d5f6d520b4b9ae82bac2
aniruddhafb <aniruddhavikharankarfb@gmail.com>
2023-12-08 23:43:55 +0530

    initial setup, bootsrap and chakra

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/bootstrap.css'
+import '@/styles/bootstrap-un.css'
+import '@/styles/font-awesome.min.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
