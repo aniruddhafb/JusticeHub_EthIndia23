07720d46f19b5e5d0b206e2a26cb72907bf69fea
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-12-08 23:58:15 +0530

    initializing hardhat

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
-import '@/styles/bootstrap.css'
-import '@/styles/bootstrap-un.css'
-import '@/styles/font-awesome.min.css'
+import "@/styles/bootstrap.css";
+import "@/styles/bootstrap-un.css";
+import "@/styles/font-awesome.min.css";
+import { ethers } from "ethers";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const connect_wallet = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    console.log(signer);
+  };
+  useEffect(() => {
+    connect_wallet();
+  }, []);
+  return <Component {...pageProps} />;
 }
