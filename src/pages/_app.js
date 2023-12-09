8d2582e754e51527c05f213cb7fb286ce8528153
aniruddhafb <aniruddhavikharankarfb@gmail.com>
2023-12-09 12:29:41 +0530

    several ui changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,28 +7,34 @@ import '@/styles/responsive.css'
 
 // other imports
 import { ethers } from "ethers";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import crypto from "crypto";
 import { AnonAadhaarProvider } from "anon-aadhaar-react";
 
+// anon adhaar id 
 const app_id = process.env.NEXT_PUBLIC_APP_ID || "";
 
 export default function App({ Component, pageProps }) {
-
+  const[walletAddress, setWalletAddress]= useState();
+  
   const connect_wallet = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     await provider.ready;
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
-    const walletAddress = await signer.getAddress();
-    console.log({ walletAddress });
+    const wallet_address = await signer.getAddress();
+    setWalletAddress(wallet_address);
   };
 
   return (
     <ChakraProvider>
-          <AnonAadhaarProvider _appId={app_id}>
-      <Component {...pageProps} connect_wallet={connect_wallet}/>
+      <AnonAadhaarProvider _appId={app_id}>
+        <Component
+          {...pageProps}
+          connect_wallet={connect_wallet}
+          walletAddress={walletAddress}
+        />
       </AnonAadhaarProvider>
     </ChakraProvider>
   );
